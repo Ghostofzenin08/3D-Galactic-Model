9d3a58c8a90f866220e9a4d3cbff6466724ee522
Ghostofzenin08 <harshalsonawane308@gmail.com>
2026-09-19 09:55:04 +0530

    Initialize Game loop

--- src/main.py
+++ src/main.py
@@ -7,7 +7,18 @@ WINDOW_HEIGHT = 800
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-pygame.diplay.set_caption("Galactic Model")
+pygame.display.set_caption("Galactic Model")
 
 running = True
 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+
+pygame.quit()
