2d5d9f7b71104cf09bf7fac99023912eb73c5f68
Ghostofzenin08 <harshalsonawane308@gmail.com>
2026-09-19 15:03:15 +0530

    Create Class for Stars system

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,14 +7,37 @@ WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
 TARGET_FPS = 60
 
+class Stars:
+    def __init__(self, x, y, velocity_x, velocity_y, mass, radius, brightness, temperature, color, age):
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(velocity_x, velocity_y)
+        self.acceleration = pygame.Vector2(0, 0)
+        self.mass = mass
+        self.radius = radius
+        self.brightness = brightness
+        self.temperature  = temperature
+        self.color = color
+        self.age = age
 
+    def update(self, delta_time):
+        self.velocity += self.acceleration * delta_time
+        self.position += self.velocity  * delta_time
+
+    def draw(self, surface):
+        display_color = tuple(int(value * self.brightness) for value in self.color)
+        pygame.draw.circle(surface, display_color, self.position, self.radius)
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 clock = pygame.time.Clock()
 font = pygame.font.Font(None,28)
 
+stars = []
+for __ in range(STAR_COUNT):
+    x = random.radint(0, WINDOW_WIDTH)
+    y = random.radint(0, WINDOW_HEIGHT)
 
+    stars.append(Stars(x, y, ))
 
 pygame.display.set_caption("Galactic Model")
 
