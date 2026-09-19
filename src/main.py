f17b03a3683849fbd7d24a10fb843fd09907b403
Ghostofzenin08 <harshalsonawane308@gmail.com>
2026-09-19 09:06:44 +0530

    Create pygame Window for Model set-up

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+pygame.diplay.set_caption("Galactic Model")
+
+running = True
+
