65abbdde931489e09e79835c4b990a4a5f030ac8
Ghostofzenin08 <harshalsonawane308@gmail.com>
2026-09-21 11:25:45 +0530

    Create pygame window with fixing bugs and errors

--- src/spiral_3d.py
+++ src/spiral_3d.py
@@ -5,3 +5,40 @@ import sys
 
 import pygame
 
+
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+TARGET_FPS = 60
+
+STAR_COUNT = 5500
+
+
+
+def main():
+    pygame.init()
+    try:
+        screen =  pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    except pygame.error:
+        if "SDL_VIDEODRIVER" not in os.environ:
+            os.environ["SDL_VIDEODRIVER"] = "dummy"
+            pygame.quit()
+            pygame.init()
+            screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        else:
+            raise
+        pygame.display.set_caption("Galactic_Model")
+        clock = pygame.time.Clock()
+        font = pygame.font.Font(None, 24)
+    
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running  = False
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
